adccd1366c9c0a28f4381a029c127fbe602fab1f
Edward Bebbington <edward.bebbington@intercity.technology>
2020-07-08 23:05:14 +0100

    Added tests for csv to json

--- src/csv_to_json.ts
+++ src/csv_to_json.ts
@@ -1,10 +1,25 @@
-export function CSVtoJSON (csvData: string): Array<Array<string>> {
-  const titles = csvData.split("\n")[0].split(',')
-  const rows = csvData.split("\n").splice(0, 1)
-  let json = []
-  json.push(titles)
-  rows.forEach(row => {
-    json.push(row)
+export function CSVtoJSON (csvData: string): Array<Array<string|number>> {
+  const splitCsv = csvData.split("\n");
+  const titles = splitCsv[0].replace(/, /g, "-").split('-')
+  splitCsv.shift();
+  if (splitCsv[splitCsv.length - 1] === "") {
+    splitCsv.pop()
+  }
+  let rows: Array<Array<string>> = [];
+  splitCsv.forEach(rowString => {
+
+    rows.push(rowString.replace(/, /g, "-").split('-'))
+  })
+  if (!rows || !rows.length) {
+    return [
+      titles
+    ]
+  }
+  let json: Array<Array<string|number>> = [];
+  json[0] = titles
+  rows.forEach((row, i) => {
+    json[i + 1] = row
   });
+  console.log(json)
   return json
 }
