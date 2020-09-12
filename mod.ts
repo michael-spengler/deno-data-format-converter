44908b779a1f3790ad1b9b80f12b7f9baf4ad7ba
michael spengler <43786652+michael-spengler@users.noreply.github.com>
2020-09-13 00:44:21 +0200

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 export { JSONtoCSV } from "./src/json_to_csv.ts";
 
-export { CSVtoJSON } from "./src/csv_to_json";
+export { CSVtoJSON } from "./src/csv_to_json.ts";
 
 export { JSONtoXML } from "./src/json_to_xml.ts"
